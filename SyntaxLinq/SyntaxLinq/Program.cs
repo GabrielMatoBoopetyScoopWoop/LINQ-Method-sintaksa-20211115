b8b9dc4e6d386877aafd60be8f87f82b28516a78
GabrielMato <58472077+GabrielMatoBoopetyScoopWoop@users.noreply.github.com>
2021-11-15 08:51:29 +0100

    Add files via upload

---
+++ SyntaxLinq/SyntaxLinq/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        string[] rijeci = {"ime", "prezime", "nekarecenica"};
+
+        var result = rijeci.Where(a => a.Contains("ime"));
+
+        foreach (var odgovor in result)
+        {
+            Console.WriteLine(odgovor);
+        }
+        Console.ReadKey();
+    }
+}
